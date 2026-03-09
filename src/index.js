0178a48b4fdd4ab63bec6a5369888e7b46d3f67b
Thisura_Jit <thisura.thebuwana@dialog.lk>
2026-03-09 21:22:33 +0530

    test commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+// app.listen(3000, () => console.log('Server running on port 3000 test'));
+app.listen(3000, () => console.log('Server running on port 3000 test'));
